91778371e80a9d63e23cedb4fe8eb21b3f7e8ae3
sumit <roundup414@gmail.com>
2024-07-17 02:08:27 +0530

    make website responsive

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
 
@@ -11,10 +12,10 @@ function Navbar() {
     <div className='navbar'>
         <h1 className='logo'>HungerEats</h1>
         <ul className='navbar-menu'>
-          <li onClick={()=>{setmenu("home")}} className={menu==="home"?"active":""}>home</li>
-          <li onClick={()=>{setmenu("menu")}} className={menu==="menu"?"active":""}>menu</li>
-          <li onClick={()=>{setmenu("mobile-app")}} className={menu==="mobile-app"?"active":""}>mobile-app</li>
-          <li onClick={()=>{setmenu("contact-us")}} className={menu==="contact-us"?"active":""}>contact us</li>
+          <Link to='/' onClick={()=>{setmenu("home")}} className={menu==="home"?"active":""}>home</Link>
+          <a href='#explore-menu' onClick={()=>{setmenu("menu")}} className={menu==="menu"?"active":""}>menu</a>
+          <a href='#app-download' onClick={()=>{setmenu("mobile-app")}} className={menu==="mobile-app"?"active":""}>mobile-app</a>
+          <a href='#footer' onClick={()=>{setmenu("contact-us")}} className={menu==="contact-us"?"active":""}>contact us</a>
         </ul>
         <div className='navbar-right'>
           <SearchIcon></SearchIcon>
