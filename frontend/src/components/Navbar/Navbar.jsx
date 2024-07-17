240dcfd9dfce4bfd1824001f0bd982b21dd8aa1c
sumit <roundup414@gmail.com>
2024-07-17 18:28:11 +0530

    craeted cart page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 
 function Navbar({setShowLogin}) {
 
   const [menu,setmenu] = useState("home");
+  const {getTotalCartAmount} = useContext(StoreContext);
 
   return (
     <div className='navbar'>
-        <h1 className='logo'>HungerEats</h1>
+        <Link to='/'><h1 className='logo'>HungerEats</h1></Link>
         <ul className='navbar-menu'>
           <Link to='/' onClick={()=>{setmenu("home")}} className={menu==="home"?"active":""}>home</Link>
           <a href='#explore-menu' onClick={()=>{setmenu("menu")}} className={menu==="menu"?"active":""}>menu</a>
@@ -20,8 +22,8 @@ function Navbar({setShowLogin}) {
         <div className='navbar-right'>
           <SearchIcon></SearchIcon>
           <div className='navbar-search-icon'> 
-            <ShoppingBasketIcon></ShoppingBasketIcon>
-            <div className='dot'></div>
+            <Link to='/cart'><ShoppingBasketIcon></ShoppingBasketIcon></Link>
+            <div className={getTotalCartAmount()===0?"":"dot"}></div>
           </div>
           <button onClick={()=>setShowLogin(true)}>Sign In</button>
         </div>
