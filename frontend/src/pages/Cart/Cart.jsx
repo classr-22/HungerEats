b59d7f35c6874a75ce6d117ac1de5e59a1e4fc99
sumit <roundup414@gmail.com>
2024-07-26 23:14:16 +0530

    fetching data from database and displaying on frontend

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 function Cart() {
 
-  const {cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext);
+  const {cartItems,food_list,removeFromCart,getTotalCartAmount,url} = useContext(StoreContext);
   const navigate = useNavigate();
 
   return (
@@ -26,7 +26,7 @@ function Cart() {
             return(
               <div>
                 <div className='cart-items-title cart-items-item'>
-                  <img src={item.image}/>
+                  <img src={url+"/images/"+item.image}/>
                   <p>{item.name}</p>
                   <p>{item.price}</p>
                   <p>{cartItems[item._id]}</p>
