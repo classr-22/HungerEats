68f8004ba4dbabb83e8868427798ea7cc97f7840
sumit <roundup414@gmail.com>
2024-07-28 17:46:57 +0530

    integrated listOrders api with frontend

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './Orders.css'
+import { useState } from 'react'
+import {toast} from 'react-toastify';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { assets } from '../../assets/assets.js';
 
-const Orders = () => {
+const Orders = ({url}) => {
+
+  const [orders,setOrders] =useState([]);
+
+  const fetchAllOrders=async(req,res)=>{
+    const response = await axios.get(url+"/api/order/list");
+    if(response.data.success){
+      setOrders(response.data.data);
+    }else{
+      toast.error("Error");
+    }
+  }
+
+  useEffect(()=>{
+    fetchAllOrders();
+  },[])
+
   return (
-    <div>Orders</div>
+    <div className='order add'>
+      <h3>Order page</h3>
+      <div className="order-list">
+        {orders.map((order,index)=>(
+          <div key={index} className="order-item">
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              <p className='order-item-food'>
+                {order.items.map((item,index)=>{
+                  if(index===order.items.length-1){
+                    return item.name+" x "+item.quantity
+                  }else{
+                    return item.name+" x "+item.quantity+", "
+                  }
+                })}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
