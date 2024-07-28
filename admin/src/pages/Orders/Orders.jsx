13486c0bf6b6c6b26f6a3db0419b1280826be281
sumit <roundup414@gmail.com>
2024-07-28 19:07:05 +0530

    successfully completed food delivery app

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -19,6 +19,16 @@ const Orders = ({url}) => {
     }
   }
 
+  const statusHandle=async(event,orderId)=>{
+    const response = await axios.post(url+"/api/order/status",{
+      orderId,
+      status: event.target.value
+    });
+    if(response.data.success){
+      await fetchAllOrders();
+    }
+  }
+
   useEffect(()=>{
     fetchAllOrders();
   },[])
@@ -49,7 +59,7 @@ const Orders = ({url}) => {
             </div>
             <p>Items : {order.items.length}</p>
             <p>${order.amount}</p>
-            <select>
+            <select onChange={(event)=>statusHandle(event,order._id)} value={order.status}>
               <option value="Food Processing">Food Processing</option>
               <option value="Out for delivery">Out for delivery</option>
               <option value="Delivered">Delivered</option>
